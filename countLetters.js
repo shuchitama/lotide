da27c45a2e85ab44991d8280d980d998fb6523cb
Shuchita <shuchita@live.com>
2020-02-05 22:03:25 +0000

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = function(sentence) {
+  
+};
+
+const result1 = countLetters("lighthouse in the house");
+console.log(result1);
+
+assertEqual(result1["l"], 1);
+assertEqual(result1["i"], 2);
+assertEqual(result1["g"], 1);
+assertEqual(result1["h"], 4);
+assertEqual(result1["t"], 2);
+assertEqual(result1["o"], 2);
+assertEqual(result1["u"], 2);
+assertEqual(result1["s"], 2);
+assertEqual(result1["e"], 3);
+assertEqual(result1["n"], 1);
