a27a64eace1bf5bf4238f3022ef8a92b3291c877
Shuchita <shuchita@live.com>
2020-02-05 22:46:54 +0000

    Initial commit

---
+++ letterPositions.js
@@ -0,0 +1,26 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello")["h"], [0]);
+assertArraysEqual(letterPositions("hello")["e"], [1]);
+assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
+assertArraysEqual(letterPositions("hello")["o"], [4]);
