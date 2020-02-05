54da8e14447200732e6224b72be037a307c27e42
Shuchita <shuchita@live.com>
2020-02-05 23:08:32 +0000

    Implement function to return all the indices at which each character in a string is found

--- letterPositions.js
+++ letterPositions.js
@@ -17,10 +17,30 @@ const assertArraysEqual = function(array1, array2) {
 
 const letterPositions = function(sentence) {
   const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
   return results;
 };
+console.log(letterPositions("hello"));
+console.log(letterPositions("lighthouse in the house"));
 
 assertArraysEqual(letterPositions("hello")["h"], [0]);
 assertArraysEqual(letterPositions("hello")["e"], [1]);
 assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
 assertArraysEqual(letterPositions("hello")["o"], [4]);
+
+assertArraysEqual(letterPositions("lighthouse in the house")["l"], [0]);
+assertArraysEqual(letterPositions("lighthouse in the house")["i"], [1, 11]);
+assertArraysEqual(letterPositions("lighthouse in the house")["g"], [2]);
+assertArraysEqual(letterPositions("lighthouse in the house")["h"], [3, 5, 15, 18]);
+assertArraysEqual(letterPositions("lighthouse in the house")["t"], [4, 14]);
+assertArraysEqual(letterPositions("lighthouse in the house")["o"], [6, 19]);
+assertArraysEqual(letterPositions("lighthouse in the house")["u"], [7, 20]);
+assertArraysEqual(letterPositions("lighthouse in the house")["s"], [8, 21]);
+assertArraysEqual(letterPositions("lighthouse in the house")["e"], [9, 16, 22]);
+assertArraysEqual(letterPositions("lighthouse in the house")["n"], [12]);
