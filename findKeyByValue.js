e6e7dfa6e76f796166b621e9f82d0aa725f1e8e4
Shuchita <shuchita@live.com>
2020-02-05 23:45:07 +0000

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKeyByValue = function() {
+
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "Doctor Who",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Doctor Who"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
