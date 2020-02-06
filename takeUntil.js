f0b2931e0f145d5f8a202a5ab81eb81a8b4c0362
Shuchita <shuchita@live.com>
2020-02-06 23:37:31 +0000

    Function implemented, more test cases added

--- takeUntil.js
+++ takeUntil.js
@@ -19,6 +19,8 @@ const takeUntil = function(array, callback) {
   for (const element of array) {
     if(!callback(element)){
       result.push(element);
+    } else {
+    return result;
     }
   }
   return result;
@@ -26,13 +28,26 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-// assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+// console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-// assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+// console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
 
+console.log('---');
+
+const data3 = [1, 3, 5, 2, 7, 8, 4, 6, 9, 0];
+const results3 = takeUntil(data3, x => x > 10);
+console.log(results3);
+assertArraysEqual(results3, [1, 3, 5, 2, 7, 8, 4, 6, 9, 0]);
+
+console.log('---');
+
+const data4 = [];
+const results4 = takeUntil(data4, x => x > 10);
+// console.log(results4);
+assertArraysEqual(results4, []);
