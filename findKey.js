88d7371e95dc87e0190a3e13c0aee9aa02fc7b26
Shuchita <shuchita@live.com>
2020-02-06 23:49:11 +0000

    Initial commit

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKey = function(object, callback) {
+
+};
+
+const obj1 = 
+  {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  };
+
+asserEqual(findKey(Obj1, x => x.stars === 2), "noma");
