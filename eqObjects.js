767d3f1f7b3894c008b3e811d1b4e52a5183d1a2
Shuchita <shuchita@live.com>
2020-02-06 00:27:01 +0000

    return false if the two objects don't have equal number of keys

--- eqObjects.js
+++ eqObjects.js
@@ -9,7 +9,9 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // otherwise returns false.
 const eqObjects = function(object1, object2) {
-
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  };
 };
 
 const ab = { a: "1", b: "2" };
