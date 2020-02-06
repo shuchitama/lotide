891d33f54e6232e43879060efb07f61a2e1d3288
Shuchita <shuchita@live.com>
2020-02-06 00:57:46 +0000

    recognises when a value in object1 is an array

--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // otherwise returns false.
 const eqObjects = function(object1, object2) {
@@ -13,13 +21,24 @@ const eqObjects = function(object1, object2) {
     return false;
   };
   for (const key in object1) {
+    if (Array.isArray(object1[key])) {console.log(`[${object1[key]}] is an array!`)}
     if (object2[key] !== object1[key]) {return false};
   }
   return true;
 };
 
-const ab = { a: "1", b: "2"};
-const abc = { a: "1", b: "2", c: "3" };
-const bca = { b: "2", c: "3", a: "1" };
-assertEqual(eqObjects(abc, bca), true); 
-assertEqual(eqObjects(ab, abc), false);
+// TESTING PRIMITIVE VALUES ONLY
+// const ab = { a: "1", b: "2"};
+// const abc = { a: "1", b: "2", c: "3" };
+// const bca = { b: "2", c: "3", a: "1" };
+// assertEqual(eqObjects(abc, bca), true); 
+// assertEqual(eqObjects(ab, abc), false);
+
+// TESTING PRIMITIVE AND ARRAY VALUES
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, dc); // => true
+eqObjects(cd, cd2); // => false
+assertEqual(eqObjects(cd, dc), true);
+assertEqual(eqObjects(cd, cd2), false);
