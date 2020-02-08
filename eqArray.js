bac0f23b30c51a55e73fc90faf25e5b4656a0a3d
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-07 19:59:10 -0800

    Now supports nested arrays

--- eqArray.js
+++ eqArray.js
@@ -7,9 +7,17 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) return false;
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) return false;
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (eqArrays(array1[i], array2[i]) === false) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false;
+    }
   }
   return true;
 };
@@ -19,3 +27,8 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
+
+// TESTING NESTED ARRAYS:
+assertEqual(eqArrays([[2, 3] ,[4]], [[2, 3] ,[4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
