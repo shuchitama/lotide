23453c13373321269f78a3aa1ce39aa24bcb4b33
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-07 20:03:29 -0800

    Check for multiple layers of nesting

--- eqArray.js
+++ eqArray.js
@@ -30,5 +30,6 @@ assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
 
 // TESTING NESTED ARRAYS:
 assertEqual(eqArrays([[2, 3] ,[4]], [[2, 3] ,[4]]), true);
+assertEqual(eqArrays([[2, 3, [5, 8, [0]]] ,[4]], [[2, 3, [5, 8, [0]]] ,[4]]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
