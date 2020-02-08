4db9be93334899d581fe68d2340e7c32afedda44
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-07 19:09:16 -0800

    "Supports nested objects"

--- eqObjects.js
+++ eqObjects.js
@@ -25,6 +25,16 @@ const eqObjects = function(object1, object2) {
       if (eqArrays(object1[key], object2[key]) === false) {
         return false;
       }
+    } else if (typeof object1[key] === 'object') {
+      if (object2[key] !== undefined && 
+          typeof object2[key] === 'object' && 
+          !Array.isArray(object2[key])) {
+        if (eqObjects(object1[key], object2[key]) === false) {
+          return false;
+        }
+      } else {
+        return false
+      };
     } else {
       if (object2[key] !== object1[key]) {
         return false;
@@ -34,14 +44,14 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-// TESTING PRIMITIVE VALUES ONLY
+// // TESTING PRIMITIVE VALUES ONLY
 const ab = { a: "1", b: "2"};
 const abc = { a: "1", b: "2", c: "3" };
 const bca = { b: "2", c: "3", a: "1" };
 assertEqual(eqObjects(abc, bca), true);
 assertEqual(eqObjects(ab, abc), false);
 
-// TESTING PRIMITIVE AND ARRAY VALUES
+// // TESTING PRIMITIVE AND ARRAY VALUES
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
@@ -49,3 +59,25 @@ eqObjects(cd, dc); // => true
 eqObjects(cd, cd2); // => false
 assertEqual(eqObjects(cd, dc), true);
 assertEqual(eqObjects(cd, cd2), false);
+
+// TESTING NESTED OBJECTS
+// Test : one nest, order changed
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { b: 2, a: { z: 1 } }), true);
+
+// Test: both have nests, but not the same inner object
+assertEqual(eqObjects(
+  { 
+    a: { y: 0, z: 1 }, 
+    b: 2 
+  }, 
+  { a: { z: 1 }, 
+    b: 2 
+  }
+  ), false);
+
+assertEqual(eqObjects(
+  { a: { y: 0, z: 1 },
+    b: 2 }, 
+  { a: 1, 
+    b: 2 }
+    ), false);
