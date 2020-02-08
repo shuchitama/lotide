36880469c4f554f71f20690da86f4747d8643155
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-07 20:00:32 -0800

    eqArrays now supports nested arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,12 +6,20 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2) === true) {
-    console.log(`✅ Assertion Passed: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
   }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (eqArrays(array1[i], array2[i]) === false) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
