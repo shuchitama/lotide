d8135f445d134e6fc39dc679e0efabf689c437a2
Shuchita Mahey <shuchitamahey@Shuchitas-MacBook-Pro.local>
2020-02-10 12:40:31 -0800

    refactored to use modules

--- eqArray.js
+++ eqArray.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
@@ -22,14 +16,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["1", "2", "3", "4"], ["1", "2", "3"]), false);
-
-// TESTING NESTED ARRAYS:
-assertEqual(eqArrays([[2, 3] ,[4]], [[2, 3] ,[4]]), true);
-assertEqual(eqArrays([[2, 3, [5, 8, [0]]] ,[4]], [[2, 3, [5, 8, [0]]] ,[4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+module.exports = eqArrays;
